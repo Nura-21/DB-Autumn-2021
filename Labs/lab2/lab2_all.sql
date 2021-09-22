b9a091f056f63e77ff766334c47cae452daa5ead
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2021-09-22 23:41:03 +0600

    One File with all exercises

--- Labs/lab2/lab2_all.sql
+++ Labs/lab2/lab2_all.sql
@@ -59,6 +59,7 @@ CREATE TABLE order_items(
     order_code INTEGER UNIQUE NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
     FOREIGN KEY (order_code) REFERENCES orders(code) ON DELETE CASCADE,
+    PRIMARY KEY (product_id,order_code),
     quantity INTEGER NOT NULL CHECK(quantity > 0)
 );
 
@@ -66,50 +67,78 @@ CREATE TABLE order_items(
 
 -- 3 EXERCISE
 CREATE TABLE students(
-    full_name VARCHAR PRIMARY KEY,
-    age INTEGER NOT NULL CHECK(AGE > 0 and AGE < 100),
-    birth_date DATE NOT NULL,
-    gender VARCHAR NOT NULL,
-    avg_grade INTEGER NOT NULL,
-    inf_uslf TEXT NOT NULL,
-    need_for_dorm TEXT,
-    add_inf TEXT
+    id INTEGER PRIMARY KEY,
+    full_name VARCHAR UNIQUE NULL,
+    age INTEGER NOT NULL CHECK(AGE > 0 and AGE < 100)
 );
 
+CREATE TABLE students_info(
+    student_id INTEGER UNIQUE NOT NULL,
+    birth_date DATE NOT NULL,
+    sex VARCHAR NOT NULL CHECK(sex = 'Male' or sex = 'Female'),
+    avg_grade INTEGER NOT NULL,
+    inf_uslf TEXT,
+    need_for_dorm TEXT,
+    add_inf TEXT,
+    FOREIGN KEY(student_id) REFERENCES students(id) ON DELETE CASCADE
+);
+
+CREATE TABLE participants(
+    student_id INTEGER NOT NULL,
+    lesson_id VARCHAR NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id) ON DELETE CASCADE,
+    PRIMARY KEY (student_id, lesson_id)
+);
+
+CREATE TABLE lessons(
+    id VARCHAR PRIMARY KEY,
+    name_of_lesson VARCHAR NOT NULL,
+    inst_id INTEGER NOT NULL,
+    room INTEGER NOT NULL CHECK (room > 0 and room < 301),
+    FOREIGN KEY (inst_id) REFERENCES instructors(id) ON DELETE CASCADE
+);
 CREATE TABLE instructors(
-    full_name VARCHAR PRIMARY KEY,
-    sp_lang VARCHAR NOT NULL,
-    work_exp INTEGER NOT NULL CHECK(work_exp > 0),
+    id INTEGER PRIMARY KEY,
+    full_name VARCHAR UNIQUE NOT NULL,
     poss_of_remote_les BOOLEAN
 );
 
-CREATE TABLE lesson_participants(
-    title VARCHAR NOT NULL,
-    instr_name VARCHAR NOT NULL,
-    part_studens VARCHAR NOT NULL,
-    FOREIGN KEY (instr_name) REFERENCES instructors(full_name) ON DELETE CASCADE,
-    FOREIGN KEY (part_studens) REFERENCES students(full_name) ON DELETE CASCADE,
-    room_number INTEGER CHECK(room_number > 0 and room_number < 301)
+CREATE TABLE instructors_info(
+    inst_id INTEGER UNIQUE NOT NULL,
+    sp_lang VARCHAR NOT NULL,
+    age INTEGER NOT NULL CHECK (age > 0 and age < 100),
+    work_exp INTEGER NOT NULL CHECK(work_exp > 0 and work_exp < age),
+    FOREIGN KEY (inst_id) REFERENCES instructors(id) ON DELETE CASCADE
 );
 
-INSERT INTO students VALUES ('Turdalin Nurassyl',18,to_date('21/10/2002','dd/mm/yyyy'), 'Male',4,'Love love');
-INSERT INTO students VALUES ('Adam First',21,to_date('15/12/1999','dd/mm/yyyy'), 'Male',5,'First person');
-INSERT INTO students VALUES ('Eva Second',21,to_date('09/12/1999','dd/mm/yyyy'), 'Female',5,'Second person');
-INSERT INTO students VALUES ('Ava Third',19,to_date('22/11/2001','dd/mm/yyyy'), 'Female',3,'Third Person');
 
-INSERT INTO instructors VALUES ('Beisenbek Baisakov','English, Russian, Kazakh', 10,TRUE);
-INSERT INTO instructors VALUES ('Bobur Mukhsimbaev','English, Russian, Kazakh', 15,TRUE);
-INSERT INTO instructors VALUES ('BRB','Russian!', 20,TRUE);
-INSERT INTO instructors VALUES ('Askar Dzhumadildaev','English, Russian, Kazakh', 25,TRUE);
 
-INSERT INTO lesson_participants VALUES ('ALGO','Beisenbek Baisakov','Turdalin Nurassyl',224);
-INSERT INTO lesson_participants VALUES ('ALGO','Beisenbek Baisakov','Adam First',224);
-INSERT INTO lesson_participants VALUES ('ALGO','Beisenbek Baisakov','Ava Third',224);
+INSERT INTO students VALUES (1,'Turdalin Nurassyl',18);
+INSERT INTO students VALUES (2,'Adam First',21);
+INSERT INTO students VALUES (3,'Eva Second',21);
+INSERT INTO students VALUES (4,'Ava Third',19);
 
-INSERT INTO lesson_participants VALUES ('Discrete','Askar Dzhumadildaev','Turdalin Nurassyl',241);
-INSERT INTO lesson_participants VALUES ('Discrete','Askar Dzhumadildaev','Ava Third',241);
-INSERT INTO lesson_participants VALUES ('Discrete','Askar Dzhumadildaev','Eva Second',241);
+INSERT INTO instructors VALUES (1,'Beisenbek Baisakov',TRUE);
+INSERT INTO instructors VALUES (2,'Askar Dzhumadildaev',TRUE);
+INSERT INTO instructors VALUES (3,'BRB',TRUE);
+INSERT INTO instructors VALUES (4,'Kooks NHK',TRUE);
+INSERT INTO instructors VALUES (5,'Askar Akshabaev',TRUE);
+INSERT INTO instructors VALUES (6,'Alimzhan Amanov',TRUE);
 
+INSERT INTO lessons VALUES (101,'Algo',1,205);
+INSERT INTO lessons VALUES (102,'Algo',5,206);
+INSERT INTO lessons VALUES (103,'Algo',6,207);
+INSERT INTO lessons VALUES (111,'Discrete',2,222);
+INSERT INTO lessons VALUES (121,'Russian',3,115);
+INSERT INTO lessons VALUES (135,'DB Practise',4,255);
+
+INSERT INTO participants VALUES (1,101);
+INSERT INTO participants VALUES (1,135);
+INSERT INTO participants VALUES (2,101);
+
+SELECT students.full_name,name_of_lesson,room,instructors.full_name FROM participants,students,lessons,instructors
+WHERE participants.student_id = students.id and participants.lesson_id = lessons.id and lessons.inst_id = instructors.id;
 
 
 -- 4 EXERCISE
